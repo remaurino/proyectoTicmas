d70e1254d7eb627418a8bbfa19ac44afc27f8c44
remaurino <remaurino@gmail.com>
2023-02-28 18:49:16 -0300

    añadi js

--- assets/codigo.js
+++ assets/codigo.js
@@ -1,38 +1,19 @@
-console.log('hola');
-
-// const menuBtn = document.querySelector("menu");
-// const navbar = document.querySelector("nav");
-
-//      menuBtn = addEventListener("click", () => {
-//      nav.classList.add("visible");
-//     //  menuBtn.classList.toggle("salir");
-//  })
-
-//  menuBtn.classList.toggle("salir");
+    const menuBtn = document.getElementById("menu");
+    const nav = document.querySelector("header nav");
+    const body = document.querySelector("body");
+    const main = document.querySelector("main");
+    const button = document.getElementsByClassName("button");
 
 
+     menuBtn.addEventListener("click", (event) =>{
+        nav.classList.toggle("visible");
+        body.classList.toggle("no-scroll");
+        main.classList.toggle("blur");
+    })
 
-
-    window.addEventListener("DOMContentLoaded", (event) => {
-       const menuBtn = document.getElementById("menu");
-       const nav = document.querySelector("header nav");
-       const body = document.querySelector("body");
-
-       menuBtn.addEventListener("click", (event) =>{
-         nav.classList.remove("hidden") || nav.classList.toggle("visible");
-         body.classList.toggle("no-scroll");
-       })
- 
-       nav.addEventListener("click", () => {
-         nav.classList.toggle("hidden");
-       }) 
-  } )
-
-
-
-
-
-
-
-
+  
+  
+    document.getElementById("button").onclick = function(){
+       document.getElementById("button-p").innerHTML = "Gracias por tu comentario"
+   }
 
