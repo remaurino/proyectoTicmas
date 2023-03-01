cd3984c09455bea6cce8de04954f4af2ab9c564c
remaurino <remaurino@gmail.com>
2023-03-01 20:59:07 -0300

    corregi js

--- assets/codigo.js
+++ assets/codigo.js
@@ -2,8 +2,8 @@
     const nav = document.querySelector("header nav");
     const body = document.querySelector("body");
     const main = document.querySelector("main");
-    const button = document.getElementsByClassName("button");
-
+    const skillsList = document.getElementById("skills-list")
+    const btnSkl = document.getElementsByClassName("button-skl");
 
      menuBtn.addEventListener("click", (event) =>{
         nav.classList.toggle("visible");
@@ -11,9 +11,13 @@
         main.classList.toggle("blur");
     })
 
-  
-  
-    document.getElementById("button").onclick = function(){
-       document.getElementById("button-p").innerHTML = "Gracias por tu comentario"
-   }
+     document.getElementById("nav-list").onclick = function(){
+         nav.classList.remove("visible");
+         main.classList.remove("blur")
+    }
 
+    document.getElementById("button").onclick = function(){
+        document.getElementById("button-p").innerHTML = "Gracias por tu comentario"
+    }
+
+    console.log("hola");
